5275c96ab5c9c2940672a2655a21e3fc6d5e188b
LAPTOP-R91CRERV\84869 <duong.nguyenwt16@hcmut.edu.vn>
2023-11-04 00:52:50 +0700

    LedDispMode() function added

---
+++ Lab3/Core/Inc/input_processing.h
@@ -0,0 +1,5 @@
+#ifndef INC_INPUT_PROCESSING_H_
+#define INC_INPUT_PROCESSING_H_
+void fsm_for_input_processing ( void );
+void IncreaseManualValue();
+#endif /* INC_INPUT_PROCESSING_H_ */
---
+++ Lab3/Core/Inc/led_display.h
@@ -0,0 +1,14 @@
+/*
+ * led_display.h
+ *
+ *  Created on: Nov 2, 2023
+ *      Author: 84869
+ */
+
+#ifndef INC_LED_DISPLAY_H_
+#define INC_LED_DISPLAY_H_
+
+#include "main.h"
+#include "timer.h"
+
+#endif /* INC_LED_DISPLAY_H_ */
